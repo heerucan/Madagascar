930f256f83f01798590ec29d166b8ac59239ad2d
heerucan <iwpow0307@gmail.com>
2021-10-23 04:49:34 +0900

    [Add] FCM 테스트용 프로젝트 파일

--- Madagascar/Madagascar/AppDelegate.swift
+++ Madagascar/Madagascar/AppDelegate.swift
@@ -1,12 +1,11 @@
 //
 //  AppDelegate.swift
-//  APNs
+//  Madagascar
 //
-//  Created by Thisisme Hi on 2021/10/22.
+//  Created by Thisisme Hi on 2021/10/23.
 //
 
 import UIKit
-import CoreData
 import Firebase
 import FirebaseMessaging
 
@@ -18,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
-        
         Messaging.messaging().delegate = self
         
         if #available(iOS 10.0, *) {
@@ -45,62 +43,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
     
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
     
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-    
-    // MARK: - Core Data stack
-    
-    lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-         */
-        let container = NSPersistentContainer(name: "APNs")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-    
-    // MARK: - Core Data Saving support
-    
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
-    
 }
 
 // MARK: - MessagingDelegate
@@ -116,16 +63,6 @@ extension AppDelegate: MessagingDelegate {
                 print("FCM registration token: \(token)")
             }
         }
-        
-        // 토큰 갱신 모니터링
-        print("Firebase registration token: \(String(describing: fcmToken))")
-        
-        let dataDict: [String: String] = ["token": fcmToken ?? ""]
-        NotificationCenter.default.post(
-            name: Notification.Name("FCMToken"),
-            object: nil,
-            userInfo: dataDict
-        )
     }
 }
 
@@ -139,7 +76,7 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
                                 withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions)
                                 -> Void) {
         
-        // 인증 요청을 위한 타입의 옵션들 - 소리, 뱃지, 배너 등..
+        // 포그라운드에 있을 때 어떤 거 받고 싶은데?? - 소리, 뱃지, 배너 등..
         completionHandler([[.sound, .badge, .banner]])
     }
     
@@ -155,7 +92,7 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
                 print("포그라운드에서 클릭")
                 let vc = ViewController()
                 window?.rootViewController?.present(vc, animated: true, completion: nil)
-
+                
             } else {
                 print("백그라운드에서 클릭")
             }
---
+++ Madagascar/Madagascar/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Madagascar
+//
+//  Created by Thisisme Hi on 2021/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
