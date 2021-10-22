3a5e98845fa3e670e94544104b84e2aa6716d49f
heerucan <iwpow0307@gmail.com>
2021-10-22 18:05:26 +0900

    [Feat] 푸쉬 알림 선택하면 앱으로 전환

--- APNs/APNs/AppDelegate.swift
+++ APNs/APNs/AppDelegate.swift
@@ -12,9 +12,9 @@ import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
@@ -22,54 +22,54 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         Messaging.messaging().delegate = self
         
         if #available(iOS 10.0, *) {
-          // For iOS 10 display notification (sent via APNS)
-          UNUserNotificationCenter.current().delegate = self
-
-          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-          UNUserNotificationCenter.current().requestAuthorization(
-            options: authOptions,
-            completionHandler: { _, _ in }
-          )
+            UNUserNotificationCenter.current().delegate = self
+            
+            // 앱에서 요청할 푸시 알림 권한의 종류와 관련된 옵션을 만든다. 이 경우 경고, 배지 및 소리를 요구.
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: { _, _ in }
+            )
         } else {
-          let settings: UIUserNotificationSettings =
+            let settings: UIUserNotificationSettings =
             UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-          application.registerUserNotificationSettings(settings)
+            application.registerUserNotificationSettings(settings)
         }
-
-        application.registerForRemoteNotifications()
-
+        
+        application.registerForRemoteNotifications() // -> 원격 알림을 위해 앱을 등록
+        
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    
     // MARK: - Core Data stack
-
+    
     lazy var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
          creates and returns a container, having loaded the store for the
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
-        */
+         */
         let container = NSPersistentContainer(name: "APNs")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
+                
                 /*
                  Typical reasons for an error here include:
                  * The parent directory does not exist, cannot be created, or disallows writing.
@@ -83,9 +83,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         })
         return container
     }()
-
+    
     // MARK: - Core Data Saving support
-
+    
     func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
@@ -99,67 +99,76 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
+    
 }
 
 // MARK: - MessagingDelegate
+
 extension AppDelegate: MessagingDelegate {
+    // 앱이 시작되거나 파이어베이스가 토큰을 업데이트할 때마다 파이어베이스는 앱과 동기화를 유지하기 위해 방금 추가한 메소드를 호출
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
         // 현재 등록 토큰 접근하기
         Messaging.messaging().token { token, error in
-          if let error = error {
-            print("Error fetching FCM registration token: \(error)")
-          } else if let token = token {
-            print("FCM registration token: \(token)")
-          }
+            if let error = error {
+                print("Error fetching FCM registration token: \(error)")
+            } else if let token = token {
+                print("FCM registration token: \(token)")
+            }
         }
-
+        
         // 토큰 갱신 모니터링
         print("Firebase registration token: \(String(describing: fcmToken))")
-
-          let dataDict: [String: String] = ["token": fcmToken ?? ""]
-          NotificationCenter.default.post(
+        
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
             name: Notification.Name("FCMToken"),
             object: nil,
             userInfo: dataDict
-          )
+        )
     }
 }
 
+// MARK: - UNUserNotificationCenterDelegate
+
 @available(iOS 10, *)
 extension AppDelegate: UNUserNotificationCenterDelegate {
-  // Receive displayed notifications for iOS 10 devices.
-  func userNotificationCenter(_ center: UNUserNotificationCenter,
-                              willPresent notification: UNNotification,
-                              withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions)
+    // willPresent : 앱이 포그라운드에 있는 동안 알림을 받을 때마다 호출
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions)
                                 -> Void) {
-    let userInfo = notification.request.content.userInfo
+        
+        // 인증 요청을 위한 타입의 옵션들 - 소리, 뱃지, 배너 등..
+        completionHandler([[.sound, .badge, .banner]])
+    }
+    
+    // didReceive : 푸쉬 알림 클릭하면 실행되는 메소드
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        // 여기 userinfo는 서버에서 보내주는 것...
+        let userInfo = response.notification.request.content.userInfo["hi"] as? String
+        if userInfo != nil {
+            print("앱델레케이트 푸시타고 들어옴, userinfo 있음")
+            if UIApplication.shared.applicationState == .active {
+                print("포그라운드에서 클릭")
+                let vc = ViewController()
+                window?.rootViewController?.present(vc, animated: true, completion: nil)
 
-    // With swizzling disabled you must let Messaging know about the message, for Analytics
-    // Messaging.messaging().appDidReceiveMessage(userInfo)
-
-    // ...
-
-    // Print full message.
-    print(userInfo)
-
-    // Change this to your preferred presentation option
-      completionHandler([[.sound, .badge, .banner, .list]])
-  }
-
-  func userNotificationCenter(_ center: UNUserNotificationCenter,
-                              didReceive response: UNNotificationResponse,
-                              withCompletionHandler completionHandler: @escaping () -> Void) {
-    let userInfo = response.notification.request.content.userInfo
-
-    // ...
-
-    // With swizzling disabled you must let Messaging know about the message, for Analytics
-    // Messaging.messaging().appDidReceiveMessage(userInfo)
-
-    // Print full message.
-    print(userInfo)
-
-    completionHandler()
-  }
+            } else {
+                print("백그라운드에서 클릭")
+            }
+        } else {
+            print("앱델레케이트 푸시타고 들어옴, userinfo 없음")
+        }
+        completionHandler()
+    }
+    
+    // APN은 사용자가 푸시 알림 권한을 부여하면 토큰을 생성하고 등록합니다.
+    // 이 토큰은 개별 장치를 식별하므로 알림을 보낼 수 있습니다.
+    // 파이어베이스를 사용하여 알림을 배포하면 이 코드가 해당 토큰을 파이어베이스에서 사용할 수 있게 됩니다.
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        
+        Messaging.messaging().apnsToken = deviceToken
+    }
 }
